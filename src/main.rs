1769480a9a0f30cfd398b76ab7a8f6067040cdd1
polyesterswing <kurianjojo2004@gmail.com>
2024-07-10 21:33:16 +0530

    style: run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use opencv::prelude::*;
-use opencv::{highgui, imgproc, videoio, core, Result};
 use opencv::core::VecN;
+use opencv::prelude::*;
+use opencv::{core, highgui, imgproc, videoio, Result};
 
 fn map_range(from_range: (i32, i32), to_range: (i32, i32), s: i32) -> i32 {
     to_range.0 + (s - from_range.0) * (to_range.1 - to_range.0) / (from_range.1 - from_range.0)
@@ -18,14 +18,19 @@ fn find_colors(frame: &Mat, gray: &Mat, table: &str, table_len: usize) -> Result
 
             let gray_pixel = gray.at_2d::<u8>(row, col)?;
             let new_pixel = map_range((0, 255), (0, (table_len - 1) as i32), *gray_pixel as i32);
-            out_colors.push_str(&*format!("\x1b[38;2;{};{};{}m{}", pixel[2], pixel[1], pixel[0], table.as_bytes()[new_pixel as usize] as char))
+            out_colors.push_str(&*format!(
+                "\x1b[38;2;{};{};{}m{}",
+                pixel[2],
+                pixel[1],
+                pixel[0],
+                table.as_bytes()[new_pixel as usize] as char
+            ))
         }
 
         out_colors.push_str("\n");
     }
 
     Ok(out_colors)
-
 }
 
 fn main() -> Result<()> {
@@ -50,7 +55,14 @@ fn main() -> Result<()> {
 
         if frame.size()?.width > 0 {
             let mut smaller = Mat::default();
-            imgproc::resize(&frame, &mut smaller, core::Size::new(term_size.0.into(), term_size.1.into()), 0.0, 0.0, imgproc::INTER_AREA)?;
+            imgproc::resize(
+                &frame,
+                &mut smaller,
+                core::Size::new(term_size.0.into(), term_size.1.into()),
+                0.0,
+                0.0,
+                imgproc::INTER_AREA,
+            )?;
 
             //let _ = find_colors(&smaller)?;
             //println!("{}", find_colors(&smaller)?);
@@ -60,7 +72,10 @@ fn main() -> Result<()> {
 
             highgui::imshow(window, &smaller)?;
 
-            println!("{}", find_colors(&smaller, &gray, ascii_table, ascii_table_len)?);
+            println!(
+                "{}",
+                find_colors(&smaller, &gray, ascii_table, ascii_table_len)?
+            );
         }
 
         if highgui::wait_key(10)? > 0 {
